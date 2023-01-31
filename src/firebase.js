c7a82029251f4ee6d035152352cc2cbf66772968
Grzegorz Opara <grzegorz.opara@gmail.com>
2023-01-31 15:17:30 +0100

    fixed var names

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId
+    APIKEY: process.env.REACT_APP_apiKey,
+    AUTHDOMAIN: process.env.REACT_APP_authDomain,
+    PROJECTID: process.env.REACT_APP_projectId,
+    STORAGEBUCKET: process.env.REACT_APP_storageBucket,
+    MESSAGINGSENDERID: process.env.REACT_APP_messagingSenderId,
+    APPID: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
